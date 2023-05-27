e4e088d82e22ab21a52e3344dda7ccee29074701
wizard3107 <ashishsingh9720@gmail.com>
2023-05-27 21:02:14 +0530

    added the cors and express Json

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,14 @@
 // server.js
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 db_url = "mongodb+srv://ashishsingh9720:ashu8923@cluster0.ux9ql2q.mongodb.net/?retryWrites=true&w=majority";
 const mongoose = require("mongoose");
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-
-  next();
-});
+app.use(express.json());
+app.use(cors());
 app.use(routes);
 const connect = () => {
   return mongoose.connect(db_url);
